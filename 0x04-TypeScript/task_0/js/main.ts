4a662144b18c7665f3132f9db0ddf7de7358bffa
wesam-c <wesameldeeb15@gmail.com>
2024-07-31 23:43:56 +0400

    add

--- 0x04-TypeScript/task_0/js/main.ts
+++ 0x04-TypeScript/task_0/js/main.ts
@@ -21,3 +21,24 @@ const studentList: Array<Student> = [
     studentA,
     studentB
 ];
+
+const table = document.createElement("table");
+//The <tbody> element is used to group rows of table data.
+const tableBody = document.createElement("tbody");
+
+table.appendChild(tableBody);
+
+studentList.forEach((student: Student): void => {
+    const tableRow = document.createElement('tr');
+    const tableDataForName = document.createElement('td')
+    const tableDataForLocation = document.createElement('td')
+
+    tableDataForName.textContent = student.firstName;
+    tableDataForLocation.textContent = student.location;
+
+    tableRow.append(tableDataForName);
+    tableRow.append(tableDataForLocation);
+    tableBody.append(tableRow);
+});
+
+document.body.appendChild(table);
