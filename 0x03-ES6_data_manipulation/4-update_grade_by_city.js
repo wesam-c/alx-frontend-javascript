0dda541022317b145dcf1c32120f497b3b188851
wesam-c <wesameldeeb15@gmail.com>
2024-08-02 21:17:53 +0400

    add

--- 0x03-ES6_data_manipulation/4-update_grade_by_city.js
+++ 0x03-ES6_data_manipulation/4-update_grade_by_city.js
@@ -1,17 +1,17 @@
 export default function updateStudentGradeByCity(students, city, newGrades) {
-    const defaultGrade = { grade: 'N/A'};
+  const defaultGrade = { grade: 'N/A' };
 
-    if (students instanceof Array) {
-        return students
-            .filter((student) => student.location === city)
-            .map((student) => ({
-                id: student.id,
-                firstName: student.firstName,
-                location: student.location,
-                grade: (newGrades
-                    .filter((grade) => grade.studentId === student.id)
-                .pop() || defaultGrade).grade,
-            }))
-    }
-    return [];
+  if (students instanceof Array) {
+    return students
+      .filter((student) => student.location === city)
+      .map((student) => ({
+        id: student.id,
+        firstName: student.firstName,
+        location: student.location,
+        grade: (newGrades
+          .filter((grade) => grade.studentId === student.id)
+          .pop() || defaultGrade).grade,
+      }));
+  }
+  return [];
 }
